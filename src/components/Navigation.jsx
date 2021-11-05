87d0976eab85c58cea04d30b058ed77936ccead9
tomi <dev.dreissiger@gmail.com>
2021-11-05 18:55:26 +0100

    navigation work 2

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,11 +3,58 @@ import styled from "styled-components";
 export default function Navigation() {
     return (
         <Container data-component="navigation">
-            sanyi
+
+            <Left>
+                <Logo>Bacefook</Logo>
+                <List>
+                    <Item>
+                        <Icon className='bx bx-bell'></Icon>
+                    </Item>
+                    <Item>
+                        <Icon className='bx bx-envelope'></Icon>
+                    </Item>
+                    <Item>
+                        <Icon className='bx bx-movie-play'></Icon>
+                    </Item>
+                </List>
+            </Left>
+
+            <Right>
+
+            </Right>
+
         </Container>
     );
 }
 
 const Container = styled.nav`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: #1876f2;
+    padding: 5px 5%;
+`;
+
+const Left = styled.div`
+
+`;
+
+const Logo = styled.div`
+    color: white;
+`;
+
+const List = styled.ul`
+
+`;
+
+const Item = styled.li`
+
+`;
+
+const Icon = styled.i`
+    color: white;
+`;
+
+const Right = styled.div`
 
 `;
