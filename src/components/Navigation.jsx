7410a06aa1044c7fba93956bdb43e5dd16cf3a29
tomi <dev.dreissiger@gmail.com>
2021-11-06 10:27:01 +0100

    navigation ready

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -8,18 +8,26 @@ export default function Navigation() {
                 <Logo>Bacefook</Logo>
                 <List>
                     <Item>
-                        <Icon className='bx bx-bell'></Icon>
+                        <Icon className="bx bx-bell"></Icon>
                     </Item>
                     <Item>
-                        <Icon className='bx bx-envelope'></Icon>
+                        <Icon className="bx bx-envelope"></Icon>
                     </Item>
                     <Item>
-                        <Icon className='bx bx-movie-play'></Icon>
+                        <Icon className="bx bx-movie-play"></Icon>
                     </Item>
                 </List>
             </Left>
 
             <Right>
+                <SearchBox>
+                    <SearchIcon className="bx bx-search"></SearchIcon>
+                    <SearchInput />
+                </SearchBox>
+                <ProfileBox>
+                    <ProfilePicture src="https://images.pexels.com/photos/3987001/pexels-photo-3987001.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="woman"></ProfilePicture>
+                    <ProfileOnline></ProfileOnline>
+                </ProfileBox>
 
             </Right>
 
@@ -64,5 +72,52 @@ const Icon = styled.i`
 `;
 
 const Right = styled.div`
+    display: flex;
+    align-items: center;
+`;
 
+const SearchBox = styled.div`
+    width: 20rem;    
+    display: flex;
+    align-items: center;
+    background-color: #efefef;
+    border-radius: 1rem;
+    padding: 0 1rem;
+    margin-right: 1rem;
+`;
+
+const SearchIcon = styled.i`
+    font-size: 1rem;
+    color: #505050;
+`;
+
+const SearchInput = styled.input`
+    width: 100%;
+    background-color: transparent;
+    padding: 0.5rem;
+    border: none;
+    outline: none;
+`;
+
+const ProfileBox = styled.div`
+    display: flex;
+    position: relative;
+`;
+
+const ProfilePicture = styled.img`
+    width: 2rem;
+    height: 2rem;
+    object-fit: cover;
+    border-radius: 50%;
+    cursor: pointer;
+`;
+
+const ProfileOnline = styled.div`
+    position: absolute;
+    right: 0;
+    width: 0.5rem;
+    height: 0.5rem;
+    border-radius: 50%;
+    border: 1px solid #efefef;
+    background-color: green;
 `;
